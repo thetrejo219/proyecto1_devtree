f86e166f0c8e2a74f81af232520079cc6e4e708c
thetrejo018 <juandiego6912@outlook.com>
2025-01-29 07:52:26 -0600

    first commit

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,4 +1,4 @@
-import { Link, Outlet } from "react-router-dom";
+import {  Outlet } from "react-router-dom";
 import { Toaster } from "sonner";
 import Logo from "../components/Logo";
 
--- src/views/HandleView.tsx
+++ src/views/HandleView.tsx
@@ -5,7 +5,7 @@ import HandleData from '../components/HandleData'
 export default function HandleView() {
   const params=useParams()
   const handle=params.handle!
-  const{data,error,isLoading}=useQuery({
+  const{data,error}=useQuery({
     queryFn:()=>getUserByHandle(handle),
     queryKey:['handle',handle],
     retry:1
